ff67b48951536def01e22dac1b279cbc90d9e984
Mustafa Masri <mustafamasry180@gmail.com>
2025-05-07 15:07:24 +0300

    added preperation year info

--- constants/index.ts
+++ constants/index.ts
@@ -15,6 +15,12 @@ export const EDUCATIONS = [
     studyField: "Computer Engineering",
     range: "2023 - 2027",
   },
+  {
+    name: "Language preperation",
+    degree: "C1 Turkish",
+    studyField: "Turkish Language",
+    range: "2022 - 2023",
+  }
 ];
 
 export const CHANGELOGS = [
